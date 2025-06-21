0970cbfa833f7e92725b36e489897b51c9664143
goutham <143820624+goutham-ms@users.noreply.github.com>
2025-06-21 19:19:30 +0530

    initial commit

---
+++ Backend/src/main/java/com/fabipl/fab11/Fab11Application.java
@@ -0,0 +1,13 @@
+package com.fabipl.fab11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fab11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fab11Application.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/fabipl/fab11/Fab11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabipl.fab11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fab11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
